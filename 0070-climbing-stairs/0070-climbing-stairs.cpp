4e795cfa08bc87abc2a4672abcbfd5a40aa89f69
utkarsh1906-keshari <utkarshvns1906@gmail.com>
2026-09-08 11:41:59 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.49%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,49 +1,52 @@
 class Solution {
 public:
-    // int Helper(int n , vector<int>&dat){
-    //      if(n==-1){
-    //       return 0;
-    //      }
-    //      if(n==0){
-    //       return 1;
-    //      }
+// Backward Approach
+    int Helper(int n , vector<int>&dat){
+         if(n==-1){
+          return 0;
+         }
+         if(n==0){
+          return 1;
+         }
     
-    //      if(dat[n]==0){
-    //       dat[n]= Helper(n-2,dat)+ Helper(n-1,dat);  
-            
-    //      }
-    //      return dat[n];
-    
-    // }
-
-    // int climbStairs(int n) {
-    
-    // vector<int>dat(n+1);
-    // return Helper(n,dat);
-        
-        
-        
-    // }
-    int Helper(int i , int n , vector<int>&dat){
-      
-        if(i==n){
-            return 1;
-        }
-        if(i>n){
-            return 0;
-        }
-
-         if(dat[i]==0){
-          dat[i]= Helper(i+2,n ,dat)+ Helper(i+1,n ,dat);  
+         if(dat[n]==0){
+          dat[n]= Helper(n-2,dat)+ Helper(n-1,dat);  
             
          }
-        return dat[i] ; 
-    }
+         return dat[n];
     
-    int climbStairs(int n){
-        vector<int>dat(n+1,0);
-    return Helper(0,n,dat);
+    }
+
+    int climbStairs(int n) {
+    
+    vector<int>dat(n+1);
+    return Helper(n,dat);
+        
+        
         
     }
+
+    // //Forward Approach
+    // int Helper(int i , int n , vector<int>&dat){
+      
+    //     if(i==n){
+    //         return 1;
+    //     }
+    //     if(i>n){
+    //         return 0;
+    //     }
+
+    //      if(dat[i]==0){
+    //       dat[i]= Helper(i+2,n ,dat)+ Helper(i+1,n ,dat);  
+            
+    //      }
+    //     return dat[i] ; 
+    // }
+    
+    // int climbStairs(int n){
+    //     vector<int>dat(n+1,0);
+    // return Helper(0,n,dat);
+        
+    // }
 
 };
